e553352cf332091b3856ba66010a50d482f0305e
Julia <juliapinheiro620@gmail.com>
2025-01-21 13:36:14 -0300

    Mudanças

--- src/main/java/br/com/juliasilva/main/repository/LoginRepository.java
+++ src/main/java/br/com/juliasilva/main/repository/LoginRepository.java
@@ -6,10 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
 
-
 public interface LoginRepository extends JpaRepository<EntityRegister, UUID> {
     Optional<EntityRegister> findByEmail(String email);
-
-
-
 }
